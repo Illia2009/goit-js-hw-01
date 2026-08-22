584e4d7385ce6b780fc1e4de14eb1097dd90c4fe
Illia2009 <gavrylchykherson@gmail.com>
2026-08-22 23:57:27 +0200

    Обновление проекта

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@
 
 function makeTransaction(quantity, pricePerDroid) {
     const total = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
+    return `You ordered ${quantity} droids worth ${total} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
